41bca42495b6a44cc109ce68ac80e5b0262d99ec
Parsa_Shabanpour <parsash1380@gmail.com>
2023-01-24 12:10:04 +0330

    Feature: add header.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import React from "react";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+} from "./HeaderStyles";
+
+const Header = () => (
+  <Container>
+    {/*<Div1>*/}
+    {/*  <Link href="/">*/}
+    {/*    <a style={{ display: "flex", alignItems: "center", color: "white" }} />*/}
+    {/*  </Link>*/}
+    {/*</Div1>*/}
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#skills">
+          <NavLink>Skills</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/parsashhub">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      {/*<SocialIcons href="https://www.linkedin.com/in/lordarcadius/">*/}
+      {/*  <AiFillLinkedin size="3rem" />*/}
+      {/*</SocialIcons>*/}
+      {/*<SocialIcons href="https://www.instagram.com/lordarcadius">*/}
+      {/*  <AiFillInstagram size="3rem" />*/}
+      {/*</SocialIcons>*/}
+    </Div3>
+  </Container>
+);
+
+export default Header;
