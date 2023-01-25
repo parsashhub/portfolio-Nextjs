a858bd77de04ab6d41f3f4d704d57096c07f1ef2
Parsa_Shabanpour <parsash1380@gmail.com>
2023-01-25 20:22:01 +0330

    Feature: add the skills part of the portfolio.

---
+++ src/components/Technologies/Skills.js
@@ -0,0 +1,42 @@
+import {
+  SiGit,
+  SiHtml5,
+  SiJavascript,
+  SiNextDotJs,
+  SiRedux,
+} from "react-icons/si";
+import { SiReact } from "react-icons/si";
+
+export const Skills = [
+  {
+    slug: "HTML, CSS, SASS",
+    Component: SiHtml5,
+    title: "HTML, CSS, SASS",
+    // Description: () => <></>,
+  },
+  {
+    slug: "JavaScript",
+    Component: SiJavascript,
+    title: "JavaScript",
+  },
+  {
+    slug: "Reactjs",
+    Component: SiReact,
+    title: "Reactjs",
+  },
+  {
+    slug: "NextJs",
+    Component: SiNextDotJs,
+    title: "NextJs",
+  },
+  {
+    slug: "Redux-Toolkit",
+    Component: SiRedux,
+    title: "Redux-Toolkit",
+  },
+  {
+    slug: "Git",
+    Component: SiGit,
+    title: "Git",
+  },
+];
