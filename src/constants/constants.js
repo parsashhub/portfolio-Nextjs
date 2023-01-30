62b4a9e22ea6dffbb528acf65cb537d97c9db5f7
Parsa_Shabanpour <parsash1380@gmail.com>
2023-01-30 17:27:11 +0330

    Update: update projects config.

---
+++ src/constants/constants.js
@@ -0,0 +1,65 @@
+export const projects = [
+  // {
+  //   title: "Covid-19 Status App",
+  //   description:
+  //     "COVID-19 Status is a free, open-source Android application that shows the current state of COVID-19 in India and the world. It has a beautiful, simple, fast, and responsive UI.",
+  //   image: "/images/covid.jpg",
+  //   tags: ["Java", "XML", "Firebase", "Rest API"],
+  //   source: "https://github.com/Coders-Of-XDA-OT/covid19-status-android",
+  //   visit: "https://project.vipuljha.com/covid/",
+  //   id: 0,
+  // },
+  {
+    title: "Dara — FinTech",
+    description:
+      "A financial and credit provider which is developed with React(PWA), Redux, and Material UI.",
+    image: "/images/abs.jpg",
+    tags: ["HTML", "MUI", "Reactjs", "Redux"],
+    visit: "https://daracredit.com/",
+    id: 2,
+  },
+  {
+    title: "Shahed University Admin Dashboard",
+    description:
+      "A panel which is available for both students and Uni’s admin. Furthermore, the project was developed with HTML, CSS, Js, jQuery, and bootstrap.",
+    image: "/images/covid.jpg",
+    tags: ["HTML", "CSS", "Bootstrap", "Js", "JQuery"],
+    visit: "http://shahed.ac.ir/pages/home.aspx",
+    id: 1,
+  },
+  {
+    title: "Ibaar — An intelligent transportation system",
+    description:
+      "Ibaar is a bridge between freight owners, freight companies, and drivers to order and transport freight. In addition, Ibaar has a PWA and Android APPs for drivers and a panel for different kinds of users with a variety of roles, which is designed and developed with React, Redux-toolkit, Material UI, tailwind, locations and Mapbox. ",
+    image: "/images/eb.jpg",
+    tags: ["HTML", "MUI", "Reactjs", "Redux-Toolkit", "Tailwind"],
+    visit: "https://www.ibaar.ir/",
+    id: 3,
+  },
+  {
+    title: "Ibaar Pay — FinTech and Logistics",
+    description:
+      "Ibaar pay  is a smart and integrated solution, designed with the aim of managing the logistics payments of cargo owners as well as integrating transport documents and fare payment. However, this project has a panel for the variety of roles with different access levels which is developed with React, Redux-toolkit,Material UI and tailwind.",
+    image: "/images/snapit.jpg",
+    tags: ["HTML", "MUI", "Reactjs", "Redux-Toolkit", "Tailwind"],
+    visit: "https://ibaarpay.com/",
+    id: 3,
+  },
+  {
+    title: "Naft Company Monitoring system",
+    description:
+      "A Web app which is developed by HTML, CSS, Js, jQuery, bootstrap, and GoJs.",
+    image: "/images/covid.jpg",
+    tags: ["HTML", "CSS", "JS", "JQuery", "GoJs"],
+    id: 0,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2016, text: "Started my journey as a new developer." },
+  { year: 2017, text: "Worked intensively on ROMs & Kernels." },
+  { year: 2018, text: "Learned Android app development." },
+  { year: 2019, text: "Finished Diploma in Computer Engineering." },
+  { year: 2020, text: "Got a scholarship from the Delhi government." },
+  { year: 2021, text: "Still trying to figure out this weird year." },
+];
